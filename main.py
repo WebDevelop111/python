42f1c460964c5706a2987a1ad047faa7ad51c23d
WebDevelop111 <146223948+WebDevelop111@users.noreply.github.com>
2023-10-03 13:19:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-main.py
+print("""
+  \u001b[20A_-o#&&*''''?d:>b\n
+          _o/"`''  '',, dMF9MMMMMHo_
+       .o&#'        `"MbHMMMMMMMMMMMHo.
+     .o"" '         vodM*$&&HMMMMMMMMMM?.
+    ,'              $M&ood,~'`(&##MMMMMMH\n\
+   /               ,MMMMMMM#b?#bobMMMMHMMML
+  &              ?MMMMMMMMMMMMMMMMM7MMM$R*Hk
+ ?$.            :MMMMMMMMMMMMMMMMMMM/HMMM|`*L
+|               |MMMMMMMMMMMMMMMMMMMMbMH'   T,
+$H#:            `*MMMMMMMMMMMMMMMMMMMMb#}'  `?
+]MMH#             ""*""""*#MMMMMMMMMMMMM'    -\n\
+MMMMMb_                   |MMMMMMMMMMMP'     :\n\
+HMMMMMMMHo                 `MMMMMMMMMT       .\n\
+?MMMMMMMMP                  9MMMMMMMM}       -\n\
+-?MMMMMMM                  |MMMMMMMMM?,d-    '\n\
+ :|MMMMMM-                 `MMMMMMMT .M|.   :\n\
+  .9MMM[                    &MMMMM*' `'    .\n\
+  ':9MMk                    `MMM#"        '-\n'
+     '&M}                     `          .-\n\
+      `&.                             .\n\
+        `~,   .                     ./\n\
+            . _                  .-\n\
+              --._,dd###pp="""  """')
+
+print(""" 
+  \u001b[20A_v->#H#P? "':o<>\_
+          .,dP` `''  "'-o.+H6&MMMHo_
+        oHMH9'         `?&bHMHMMMMMMHo.
+      oMP"' '           ooMP*#&HMMMMMMM?.
+    ,M*          -     `*MSdob//`^&##MMMH\n
+   d*'                .,MMMMMMH#o>#ooMMMMMb
+  HM-                :HMMMMMMMMMMMMMMM&HM[R\n
+ d"Z\.               9MMMMMMMMMMMMMMMMM[HMM|:
+-H    -              MMMMMMMMMMMMMMMMMMMbMP' :
+:??Mb#               `9MMMMMMMMMMMMMMMMMMH#! .
+': MMMMH#,              "*""""`#HMMMMMMMMMMH  '-\n'\
+'||MMMMMM6\.                    {MMMMMMMMMH'  ':\n'\
+':|MMMMMMMMMMHo                 `9MMMMMMMM'   '.\n'\
+'. HMMMMMMMMMMP'                 !MMMMMMMM   '`\n'\
+- `#MMMMMMMMM                   HMMMMMMM*,/  :\n\
+ :  ?MMMMMMMF                   HMMMMMM',P' :\n\
+  .  HMMMMR'                    {MMMMP' ^' -\n\
+   : `HMMMT                     iMMH'     .'\n\
+    -.`HMH                               .\n\
+      -:*H                            . '\n\
+        -`\,,    .                  .-\n\
+          ' .  _                 .-`\n\
+              '`~\.__,obb#q==~'''\n\
+              """)
